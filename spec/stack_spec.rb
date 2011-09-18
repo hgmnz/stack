069d24edcc7288437ceaac4ccbf5a3a6f50439e6
Harold Giménez <hgimenez@thoughtbot.com>
2011-09-18 12:33:49 -0400

    Some stack specs

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -3,18 +3,37 @@ require 'stack'
 describe Stack do
   let(:stack)  { Stack.new }
 
-  it 'starts out empty' do
-    stack.should be_empty
+  context 'by default' do
+    it 'starts out empty' do
+      stack.empty?.should be_true
+    end
   end
 
-  it 'it can be pushed an element' do
-    stack.should be_empty
-    stack.push(1)
-    stack.should have(1).element
+  context 'pushing elements' do
+    it 'can be pushed an element' do
+      stack.should be_empty
+      stack.push(1)
+      stack.size.should == 1
+    end
+
+    it 'can be pushed many elements at once' do
+      stack.should be_empty
+      stack.push(1, 2, 3, 4, 5)
+      stack.size.should == 5
+    end
   end
 
-  it 'allows you to pop the last pushed element' do
-    stack.push(:foo)
-    stack.pop.should == :foo
+  context 'popping elements' do
+    it 'allows you to pop the last pushed element' do
+      stack.push(:foo, :bar)
+      stack.pop.should == :bar
+      stack.pop.should == :foo
+      stack.should be_empty
+    end
+
+    it 'raises when popping an empty stack' do
+      stack.should be_empty
+      expect { stack.pop }.to raise_error(Stack::EmptyStackError)
+    end
   end
 end
