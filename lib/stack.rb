dbb9bbaace8a37f69bde0f8146e5bc6a07113eab
Harold Giménez <hgimenez@thoughtbot.com>
2011-09-18 12:35:20 -0400

    Initial solution without refactoring

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,2 +1,31 @@
 class Stack
+
+  class EmptyStackError < StandardError; end
+
+  def initialize
+    @data = []
+  end
+
+  def push(*elements)
+    elements.each do |element|
+      @data << element
+    end
+  end
+
+  def pop
+    raise EmptyStackError if empty?
+    element = @data[-1]
+    @data.delete_at(-1)
+
+    element
+  end
+
+  def empty?
+    @data.empty?
+  end
+
+  def size
+    @data.size
+  end
+
 end
